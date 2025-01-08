2d36006a0dcb753bd3101efcf921c92b05c4a78d
Abror <70596977+Abror4544@users.noreply.github.com>
2025-01-08 16:35:08 +0500

    Initial commit: Create a new React app and basic folder structure

---
+++ src/images.d.ts
@@ -0,0 +1,9 @@
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.svg';
+declare module '*.gif';
+declare module '*.webp';
+declare module '*.avif';
+declare module '*.ico';
+declare module '*.bmp';
